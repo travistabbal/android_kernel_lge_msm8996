95403ea76d8c50e9021bee7f91dbda1f300cd86c
Elliott Mitchell <ehem+android_kernel_lge_msm8996@m5p.com>
2017-02-11 16:23:45 -0800

    Fix two bugs found during indentation fixing
    GCC-6 generates warnings for "misleading indentation".  Many of these
    aren't real issues, but two which occurred here are definitely errors.
    
    First, in smbchg_batt_id_checker(), valid_id needed to be initialized to
    false.  Otherwise if rc was negative the function could have hit a return
    with the value being undefined.  I suspect the original author had
    planned to have the two lines inside braces, but forgot to add those.
    
    Second, in slim_change_existing_chans(), it looks like both lines were
    meant to be surrounded by braces.  I hope this was the correct fix, what
    was there before could have been reasonable depending upon what the
    author wanted.
    
    Lastly, in lgcc_charger_reginfo() there is this one suspicious line.
    From context it appears fixing the indentations is the correct approach,
    but I'm worried about this conclusion.  Could this last line have been
    meant to be part of the else clause?

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -7570,7 +7570,7 @@ static int smbchg_get_iusb(struct smbchg_chip *chip)
 static int smbchg_batt_id_checker(struct smbchg_chip *chip) {
 	union lge_power_propval lge_val = {0,};
 	int rc;
-	bool valid_id;
+	bool valid_id = false;
 
 	if (!chip->lge_batt_id_lpc)
 		chip->lge_batt_id_lpc = lge_power_get_by_name("lge_batt_id");
@@ -7582,11 +7582,7 @@ static int smbchg_batt_id_checker(struct smbchg_chip *chip) {
 			pr_err("Failed to get batt_id property\n");
 		else
 			valid_id = lge_val.intval;
-
-			return valid_id;
 	}
-
-	valid_id = false;
 
 	return valid_id;
 }
@@ -9004,9 +9000,8 @@ static void lgcc_charger_reginfo(struct work_struct *work) {
 	else
 		delay_time = CHARGING_INFORM_NORMAL_TIME;
 
-		schedule_delayed_work(&chip->charging_info_work,
-			round_jiffies_relative(msecs_to_jiffies(delay_time)));
-
+	schedule_delayed_work(&chip->charging_info_work,
+		round_jiffies_relative(msecs_to_jiffies(delay_time)));
 }
 #endif
 
